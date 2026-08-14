8d86c64152157d542708764d126312b10bb98037
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-14 12:50:40 +0500

    load_chat from db if data is present otherwise use SYSTEM_PROMPT and initialize messages in case of messages not in st.session_state

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from chat_manager import create_chat
+from chat_manager import create_chat,load_chat
+from config import SYSTEM_PROMPT
 
 st.set_page_config(
     page_title="Orbit",
@@ -12,4 +13,17 @@ st.title("Orbit")
 # INITIALIZE
 if "current_chat" not in st.session_state:
     st.session_state.current_chat = create_chat()
+
+if "messages" not in st.session_state:
+    data = load_chat(st.session_state.current_chat)
+
+    if data["messages"]:
+        st.session_state.messages = data["messages"]
+    else:
+        st.session_state.messages = [
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT,
+            }
+        ]
 
