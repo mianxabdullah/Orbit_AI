d0e5a6ffc607a0053d19d4b0b13b9684ed75daef
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-14 12:45:15 +0500

    create new chat if not available in st.session_state

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from chat_manager import create_chat
 
 st.set_page_config(
     page_title="Orbit",
@@ -7,4 +8,8 @@ st.set_page_config(
 )
 
 st.title("Orbit")
+
+# INITIALIZE
+if "current_chat" not in st.session_state:
+    st.session_state.current_chat = create_chat()
 
