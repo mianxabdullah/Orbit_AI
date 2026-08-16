065e4c4a4fd1dd8e268a6091fa8928edff73c629
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-16 13:27:25 +0500

    Changed default model from llama-3.3-70b-versatile to openai/gpt-oss-120b

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 SYSTEM_PROMPT = "You are a helpful chatbot Named Orbit. You are a large language model trained by Groq. You are helpful, creative, clever, and very friendly."
-
-DEFAULT_MODEL = "llama-3.3-70b-versatile"
+DEFAULT_MODEL = "openai/gpt-oss-120b" 
