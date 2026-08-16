2781d8db7f931fd18ec5abaa5b21820baa6f82f9
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-16 13:15:34 +0500

    Added stream_response() in groq_client.py

--- groq_client.py
+++ groq_client.py
@@ -2,6 +2,12 @@ from groq import Groq
 import streamlit as st
 
 API = st.secrets["GROQ_API_KEY"]
-
 client = Groq(api_key=API)
 
+def stream_response(messages, model):
+
+    return client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True
+    )
