25937663f916273e763f030abce7c6de93fadcf1
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-17 13:02:02 +0500

    In sidebar.py: added search chat option and listed all chats

--- sidebar.py
+++ sidebar.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from chat_manager import create_chat,load_chat
+from chat_manager import create_chat, load_chat, list_chats
 
 def render_sidebar():
     
@@ -11,3 +11,12 @@ def render_sidebar():
             data = load_chat(st.session_state.current_chat)
             st.session_state.messages = data["messages"]
             st.rerun()
+
+        # Search
+        search = st.text_input("Recents", placeholder="Search...")
+
+        # Chats
+        chats = [chat for chat in list_chats() if chat["title"] != "New Chat"]
+
+        if search:
+            chats = [chat for chat in chats if search.lower() in chat["title"].lower()] 
