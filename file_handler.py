41e245c4da204e4a221cb1a8d5041f64e68e22a0
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-19 12:45:17 +0500

    Added .pdf and .docx support

--- file_handler.py
+++ file_handler.py
@@ -1,3 +1,6 @@
+from pypdf import PdfReader
+from docx import Document
+
 def extract_text(uploaded_file):
 
     if uploaded_file is None:
@@ -9,5 +12,20 @@ def extract_text(uploaded_file):
     if file_type == "txt":
         return uploaded_file.read().decode("utf-8")
 
-    return ""
+# .pdf
+    elif file_type == "pdf":
+        reader = PdfReader(uploaded_file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+        return text
 
+# .docx
+    elif file_type == "docx":
+        doc = Document(uploaded_file)
+        return "\n".join(
+            para.text
+            for para in doc.paragraphs
+        )
+
+    return ""
