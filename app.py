e8d471123a85043b9ad896a326c2e0ab8dfbef35
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-20 13:43:11 +0500

    In app.py: If theres a document uploader than add a system msg with doc text in messages

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
-from chat_manager import create_chat,load_chat,save_chat
+from chat_manager import create_chat, load_chat, save_chat
 from groq_client import stream_response
-from config import SYSTEM_PROMPT,DEFAULT_MODEL as model
+from config import SYSTEM_PROMPT, DEFAULT_MODEL as model
 from sidebar import render_sidebar
 from file_handler import extract_text
 
-st.set_page_config(
-    page_title="Orbit",
-    page_icon="🪐",
-    layout="wide"
-)
+st.set_page_config(page_title="Orbit", page_icon="🪐", layout="wide")
 
 st.title("Orbit")
 
 # used to remove the avatars from the chat messages
-st.markdown(   
+st.markdown(
     """
 <style>
 [data-testid="stChatMessageAvatarUser"],
@@ -48,7 +44,9 @@ model, uploaded_file = render_sidebar()
 chat_data = load_chat(st.session_state.current_chat)
 
 if uploaded_file:
-    if chat_data["document_name"] != uploaded_file.name: # check if the uploaded file is different from the previous one
+    if (
+        chat_data["document_name"] != uploaded_file.name
+    ):  # check if the uploaded file is different from the previous one
         chat_data["document_name"] = uploaded_file.name
         chat_data["document_text"] = extract_text(uploaded_file)
         save_chat(st.session_state.current_chat, chat_data)
@@ -56,12 +54,12 @@ if uploaded_file:
         # reload updated chat
         chat_data = load_chat(st.session_state.current_chat)
 
-document_text = chat_data["document_text"] 
+document_text = chat_data["document_text"]
 
 if chat_data["document_name"]:
     st.sidebar.success(f"📄 {chat_data['document_name']}")
 
-# DISPLAY CHAT 
+# DISPLAY CHAT
 for message in st.session_state.messages:
 
     if message["role"] == "system":
@@ -76,7 +74,9 @@ for message in st.session_state.messages:
 # Read chat_input first so we know this run whether a message is being submitted or not. This is important because we only want to show suggestions when no message is being submitted.
 user_input = st.chat_input("Type your message...")
 
-if "suggested_prompt" in st.session_state: # check if the suggested_prompt is in the session state
+if (
+    "suggested_prompt" in st.session_state
+):  # check if the suggested_prompt is in the session state
     user_input = st.session_state.pop("suggested_prompt")
 
 # Only show suggestions if there's no message yet AND we're not
@@ -88,7 +88,7 @@ if len(st.session_state.messages) == 1 and not user_input:
         "Explain quantum computing simply.",
         "Summarize this document.",
         "Write a Python function.",
-        "Plan a 7-day trip."
+        "Plan a 7-day trip.",
     ]
 
     cols = st.columns(2)
@@ -106,19 +106,36 @@ if user_input:
             "content": user_input,
         }
     )
- 
-    chat_data = load_chat(st.session_state.current_chat) # load the chat data from the database
-    chat_data["messages"] = st.session_state.messages # update the messages in the chat data with the new messages from the session state
-    save_chat(st.session_state.current_chat, chat_data) # save the updated chat data back to the database
 
-    with st.chat_message("user"): 
+    chat_data = load_chat(
+        st.session_state.current_chat
+    )  # load the chat data from the database
+    chat_data["messages"] = (
+        st.session_state.messages
+    )  # update the messages in the chat data with the new messages from the session state
+    save_chat(
+        st.session_state.current_chat, chat_data
+    )  # save the updated chat data back to the database
+
+    with st.chat_message("user"):
         st.markdown(user_input)
 
     with st.chat_message("assistant"):
         placeholder = st.empty()
         reply = ""
 
-    messages = st.session_state.messages.copy() 
+    messages = st.session_state.messages.copy()
+
+    if document_text: # if there's a document uploaded, insert a system message with the document text at index 1 (after the user's message)
+        messages.insert(1,
+            {
+                "role": "system",
+                "content": f"""
+                                The user uploaded the following document.
+                                Use it to answer their questions.
+                                Document:{document_text}
+                            """,
+            })
 
     try:
         response = stream_response(messages, model)
